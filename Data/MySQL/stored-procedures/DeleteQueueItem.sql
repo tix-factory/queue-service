52baf869668c048cdb244528c0d5884196daca6d
WebGL3D <OgreLandBob@gmail.com>
2020-10-24 11:36:08 -0700

    Add stored procedures

---
+++ Data/MySQL/stored-procedures/DeleteQueueItem.sql
@@ -0,0 +1,16 @@
+DELIMITER $$
+USE `queue`$$
+CREATE PROCEDURE `DeleteQueueItem`(
+	IN _ID BIGINT,
+	IN _HolderID VARCHAR(36)
+)
+BEGIN
+	DELETE
+		FROM `queue`.`queues`
+		WHERE (`ID` = _ID)
+		AND (`HolderID` = _HolderID)
+		AND (`LockExpiration` >= UTC_Timestamp())
+		LIMIT 1;
+END$$
+
+DELIMITER ;
