bfd95ac78958508193b585faa43b6bd1366daa7c
WebGL3D <OgreLandBob@gmail.com>
2020-10-24 20:17:01 -0700

    Round robin database connections

--- Services/Queue/TixFactory.Queue/Implementation/DatabaseConnection.cs
+++ Services/Queue/TixFactory.Queue/Implementation/DatabaseConnection.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Data;
 using System.Linq;
 using System.Text.Json;
+using System.Threading;
 using MySql.Data.MySqlClient;
 using TixFactory.Queue.Entities;
 using TixFactory.Configuration;
@@ -11,12 +12,17 @@ namespace TixFactory.Queue
 {
 	internal class DatabaseConnection : IDatabaseConnection
 	{
+		private const int _MaxConnections = 5;
 		private readonly JsonSerializerOptions _JsonSerializerOptions;
-		private readonly ILazyWithRetry<MySqlConnection> _ConnectionLazy;
+		private readonly ISet<DatabaseConnectionWrapper> _ReadConnections;
+		private readonly ISet<DatabaseConnectionWrapper> _WriteConnections;
+		private int _ReadCount = 0;
+		private int _WriteCount = 0;
 
-		public DatabaseConnection(ILazyWithRetry<MySqlConnection> connectionLazy)
+		public DatabaseConnection(IReadOnlySetting<string> connectionString)
 		{
-			_ConnectionLazy = connectionLazy ?? throw new ArgumentNullException(nameof(connectionLazy));
+			var readConnections = _ReadConnections = new HashSet<DatabaseConnectionWrapper>();
+			var writeConnections = _WriteConnections = new HashSet<DatabaseConnectionWrapper>();
 
 			var jsonSerializerOptions = _JsonSerializerOptions = new JsonSerializerOptions
 			{
@@ -24,6 +30,12 @@ namespace TixFactory.Queue
 			};
 
 			jsonSerializerOptions.Converters.Add(new BooleanJsonConverter());
+
+			for (var n = 0; n < _MaxConnections; n++)
+			{
+				readConnections.Add(new DatabaseConnectionWrapper(connectionString));
+				writeConnections.Add(new DatabaseConnectionWrapper(connectionString));
+			}
 		}
 
 		public T ExecuteInsertStoredProcedure<T>(string storedProcedureName, IReadOnlyCollection<MySqlParameter> mySqlParameters)
@@ -36,19 +48,41 @@ namespace TixFactory.Queue
 		public IReadOnlyCollection<T> ExecuteReadStoredProcedure<T>(string storedProcedureName, IReadOnlyCollection<MySqlParameter> mySqlParameters)
 			where T : class
 		{
-			var command = new MySqlCommand(storedProcedureName, _ConnectionLazy.Value);
-			command.CommandType = CommandType.StoredProcedure;
-			command.Parameters.AddRange(mySqlParameters.ToArray());
-			return ExecuteCommand<T>(command);
+			var index = Interlocked.Increment(ref _ReadCount);
+			var dataConnectionWrapper = _ReadConnections.ElementAt(index % _MaxConnections);
+			dataConnectionWrapper.ConnectionLock.Wait();
+
+			try
+			{
+				var command = new MySqlCommand(storedProcedureName, dataConnectionWrapper.Connection);
+				command.CommandType = CommandType.StoredProcedure;
+				command.Parameters.AddRange(mySqlParameters.ToArray());
+				return ExecuteCommand<T>(command);
+			}
+			finally
+			{
+				dataConnectionWrapper.ConnectionLock.Release();
+			}
 		}
 
 		public int ExecuteWriteStoredProcedure(string storedProcedureName, IReadOnlyCollection<MySqlParameter> mySqlParameters)
 		{
-			var command = new MySqlCommand(storedProcedureName, _ConnectionLazy.Value);
-			command.CommandType = CommandType.StoredProcedure;
-			command.Parameters.AddRange(mySqlParameters.ToArray());
+			var index = Interlocked.Increment(ref _WriteCount);
+			var dataConnectionWrapper = _WriteConnections.ElementAt(index % _MaxConnections);
+			dataConnectionWrapper.ConnectionLock.Wait();
 
-			return command.ExecuteNonQuery();
+			try
+			{
+				var command = new MySqlCommand(storedProcedureName, dataConnectionWrapper.Connection);
+				command.CommandType = CommandType.StoredProcedure;
+				command.Parameters.AddRange(mySqlParameters.ToArray());
+
+				return command.ExecuteNonQuery();
+			}
+			finally
+			{
+				dataConnectionWrapper.ConnectionLock.Release();
+			}
 		}
 
 		private IReadOnlyCollection<T> ExecuteCommand<T>(MySqlCommand command)
--- Services/Queue/TixFactory.Queue/QueueOperations.cs
+++ Services/Queue/TixFactory.Queue/QueueOperations.cs
@@ -1,6 +1,4 @@
 ﻿using System;
-using System.Data;
-using MySql.Data.MySqlClient;
 using TixFactory.Configuration;
 using TixFactory.Logging;
 using TixFactory.Operations;
@@ -11,9 +9,6 @@ namespace TixFactory.Queue
 {
 	public class QueueOperations : IQueueOperations
 	{
-		private readonly ISettings _Settings;
-		private readonly ILazyWithRetry<MySqlConnection> _MySqlConnection;
-
 		public IOperation<AddQueueItemRequest, AddQueueItemResult> AddQueueItemOperation { get; }
 		public IOperation<string, int> ClearQueueOperation { get; }
 		public IOperation<LeaseQueueItemRequest, QueueItemResult> LeaseQueueItemOperation { get; }
@@ -29,9 +24,13 @@ namespace TixFactory.Queue
 				throw new ArgumentNullException(nameof(logger));
 			}
 
-			_Settings = settings ?? throw new ArgumentNullException(nameof(settings));
-			var mySqlConnection = _MySqlConnection = new LazyWithRetry<MySqlConnection>(BuildConnection);
-			var databaseConnection = new DatabaseConnection(mySqlConnection);
+			if (settings == null)
+			{
+				throw new ArgumentNullException(nameof(settings));
+			}
+
+			var connectionString = new ManufacturedSetting<string>(() => settings.QueueConnectionString, refreshOnRead: true);
+			var databaseConnection = new DatabaseConnection(connectionString);
 			var queueEntityFactory = new QueueEntityFactory(databaseConnection);
 			var queueItemEntityFactory = new QueueItemEntityFactory(databaseConnection, queueEntityFactory);
 
@@ -42,27 +41,6 @@ namespace TixFactory.Queue
 			RemoveQueueItemOperation = new RemoveQueueItemOperation(queueItemEntityFactory);
 			GetQueueSizeOperation = new GetQueueSizeOperation(queueItemEntityFactory);
 			GetHeldQueueSizeOperation = new GetHeldQueueSizeOperation(queueItemEntityFactory);
-		}
-
-		private MySqlConnection BuildConnection()
-		{
-			var connectionString = _Settings.QueueConnectionString;
-			var connection = new MySqlConnection(connectionString);
-			connection.StateChange += ConnectionStateChange;
-			connection.Open();
-
-			return connection;
-		}
-
-		private void ConnectionStateChange(object sender, StateChangeEventArgs e)
-		{
-			switch (e.CurrentState)
-			{
-				case ConnectionState.Broken:
-				case ConnectionState.Closed:
-					_MySqlConnection.Refresh();
-					return;
-			}
 		}
 	}
 }
