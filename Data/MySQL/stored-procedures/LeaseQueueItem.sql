f95b62a03544e291bb823762a2034cbcc616f71a
WebGL3D <OgreLandBob@gmail.com>
2020-10-29 08:17:59 -0700

    Add transactions to LeaseQueueItem

--- Data/MySQL/stored-procedures/LeaseQueueItem.sql
+++ Data/MySQL/stored-procedures/LeaseQueueItem.sql
@@ -5,22 +5,31 @@ CREATE PROCEDURE `LeaseQueueItem`(
 	IN _LockExpiration DATETIME
 )
 BEGIN
+	START TRANSACTION;
+
 	SET @HolderID = UUID();
+	SET @ID = (SELECT `ID`
+		FROM `queue`.`queue-items`
+		WHERE (`QueueID` = _QueueID)
+		AND (`LockExpiration` < UTC_Timestamp())
+		ORDER BY `ID` ASC
+		LIMIT 1
+		FOR UPDATE);
 
 	UPDATE `queue`.`queue-items`
 	SET
 		`HolderID` = @HolderID,
 		`LockExpiration` = _LockExpiration,
 		`Updated` = UTC_Timestamp()
-	WHERE (`QueueID` = _QueueID)
-	AND (`LockExpiration` < UTC_Timestamp())
-	ORDER BY `ID` ASC
+	WHERE (`ID` = @ID)
 	LIMIT 1;
 
 	SELECT *
 		FROM `queue`.`queue-items`
-		WHERE `HolderID` = @HolderID
+		WHERE `ID` = @ID
 		LIMIT 1;
+
+	COMMIT;
 END$$
 
 DELIMITER ;
