5dc03252cc16b75eb6b6041bc3e624860528580c
WebGL3D <OgreLandBob@gmail.com>
2020-11-22 15:44:48 -0800

    Add entity factories

--- Node/queue-service/entities/queueEntityFactory.js
+++ Node/queue-service/entities/queueEntityFactory.js
@@ -1,17 +1,56 @@
+const CacheExpiry = 60 * 1000;
+
 export default class {
 	constructor(databaseConnection) {
 		this.databaseConnection = databaseConnection;
+		this.idByNameCache = {};
+	}
+
+	getOrCreateQueueIdByName(queueName) {
+		return new Promise(async (resolve, reject) => {
+			try {
+				let queueId = await this.getQueueIdByName(queueName);
+				if (queueId) {
+					resolve(queueId);
+					return;
+				}
+
+				const cacheKey = queueName.toLowerCase();
+				this.idByNameCache[cacheKey] = queueId = await this.executeInsertStoredProcedure("InsertQueue", {
+					_Name = queueName
+				});
+
+				resolve(queueId);
+			} catch (e) {
+				reject(e);
+			}
+		});
 	}
 
 	getQueueIdByName(queueName) {
 		return new Promise(async (resolve, reject) => {
 			try {
+				const cacheKey = queueName.toLowerCase();
+				if (this.idByNameCache.hasOwnProperty(cacheKey)) {
+					resolve(this.idByNameCache[cacheKey]);
+					return;
+				}
+
 				const connection = await this.databaseConnection.getConnection();
 				const entities = await connection.executeReadStoredProcedure("GetQueueByName", {
 					_Name: queueName
 				});
 
-				resolve(entities[0] ? entities[0].ID : null);
+				const id = this.idByNameCache[cacheKey] = entities[0] ? entities[0].ID : null;
+				if (!id) {
+					setTimeout(() => {
+						if (!this.idByNameCache[cacheKey]) {
+							delete this.idByNameCache[cacheKey];
+						}
+					}, CacheExpiry);
+				}
+
+				resolve(id);
 			} catch (e) {
 				reject(e);
 			}
--- Node/queue-service/entities/queueItemEntityEntityFactory.js
+++ Node/queue-service/entities/queueItemEntityEntityFactory.js
@@ -4,6 +4,23 @@ export default class {
 		this.queueEntityFactory = queueEntityFactory;
 	}
 
+	insertQueueItem(queueName, data) {
+		return new Promise(async (resolve, reject) => {
+			try {
+				const queueId = await this.queueEntityFactory.getOrCreateQueueIdByName(queueName);
+				const connection = await this.databaseConnection.getConnection();
+				const queueItemId = await connection.executeInsertStoredProcedure("InsertQueueItem", {
+					_QueueID: queueId,
+					_Data: data
+				});
+
+				resolve(queueItemId);
+			} catch (e) {
+				reject(e);
+			}
+		});
+	}
+
 	leaseQueueItem(queueName, expirationInMilliseconds) {
 		return new Promise(async (resolve, reject) => {
 			try {
@@ -25,5 +42,106 @@ export default class {
 				reject(e);
 			}
 		});
+	}
+
+	releaseQueueItem(queueItemId, holderId) {
+		return new Promise(async (resolve, reject) => {
+			try {
+				const queueId = await this.queueEntityFactory.getQueueIdByName(queueName);
+				if (!queueId) {
+					resolve(false);
+					return;
+				}
+
+				const connection = await this.databaseConnection.getConnection();
+				const rowsModified = await connection.executeReadStoredProcedure("ReleaseQueueItem", {
+					_ID: queueItemId,
+					_HolderID: holderId
+				});
+
+				resolve(rowsModified === 1);
+			} catch (e) {
+				reject(e);
+			}
+		});
+	}
+
+	deleteQueueItem(queueItemId, holderId) {
+		return new Promise(async (resolve, reject) => {
+			try {
+				const queueId = await this.queueEntityFactory.getQueueIdByName(queueName);
+				if (!queueId) {
+					resolve(false);
+					return;
+				}
+
+				const connection = await this.databaseConnection.getConnection();
+				const rowsModified = await connection.executeReadStoredProcedure("DeleteQueueItem", {
+					_ID: queueItemId,
+					_HolderID: holderId
+				});
+
+				resolve(rowsModified === 1);
+			} catch (e) {
+				reject(e);
+			}
+		});
+	}
+
+	clearQueue(queueName) {
+		try {
+			const queueId = await this.queueEntityFactory.getQueueIdByName(queueName);
+			if (!queueId) {
+				resolve(0);
+				return;
+			}
+
+			const connection = await this.databaseConnection.getConnection();
+			const rowsModified = await connection.executeReadStoredProcedure("ClearQueue", {
+				_QueueID: queueId
+			});
+
+			resolve(rowsModified);
+		} catch (e) {
+			reject(e);
+		}
+	}
+
+	getQueueSize(queueName) {
+		try {
+			const queueId = await this.queueEntityFactory.getQueueIdByName(queueName);
+			if (!queueId) {
+				resolve(0);
+				return;
+			}
+
+			const connection = await this.databaseConnection.getConnection();
+			const count = await connection.executeCountStoredProcedure("GetQueueSize", {
+				_QueueID: queueId
+			});
+
+			resolve(count);
+		} catch (e) {
+			reject(e);
+		}
+	}
+
+	getHeldQueueSize(queueName) {
+		try {
+			const queueId = await this.queueEntityFactory.getQueueIdByName(queueName);
+			if (!queueId) {
+				resolve(0);
+				return;
+			}
+
+			const connection = await this.databaseConnection.getConnection();
+			const count = await connection.executeCountStoredProcedure("GetHeldQueueSize", {
+				_QueueID: queueId
+			});
+
+			resolve(count);
+		} catch (e) {
+			reject(e);
+		}
 	}
 };
