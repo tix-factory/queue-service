6d8cb2633e0990a55a8716f3afe3835aee7f8e9a
Bob Rose <WebGL3D@users.noreply.github.com>
2020-11-29 09:57:10 -0800

    Remove MySQL lock from LeaseQueueItem (#20)

--- Data/MySQL/stored-procedures/LeaseQueueItem.sql
+++ Data/MySQL/stored-procedures/LeaseQueueItem.sql
@@ -5,31 +5,22 @@ CREATE PROCEDURE `LeaseQueueItem`(
 	IN _LockExpiration DATETIME
 )
 BEGIN
-	START TRANSACTION;
-
 	SET @HolderID = UUID();
-	SET @ID = (SELECT `ID`
-		FROM `queue`.`queue-items`
-		WHERE (`QueueID` = _QueueID)
-		AND (`LockExpiration` < UTC_Timestamp())
-		ORDER BY `ID` ASC
-		LIMIT 1
-		FOR UPDATE);
 
 	UPDATE `queue`.`queue-items`
 	SET
 		`HolderID` = @HolderID,
 		`LockExpiration` = _LockExpiration,
 		`Updated` = UTC_Timestamp()
-	WHERE (`ID` = @ID)
+	WHERE (`QueueID` = _QueueID)
+		AND (`LockExpiration` < UTC_Timestamp())
+	ORDER BY `ID` ASC
 	LIMIT 1;
 
 	SELECT *
 		FROM `queue`.`queue-items`
-		WHERE `ID` = @ID
+		WHERE `HolderID` = @HolderID
 		LIMIT 1;
-
-	COMMIT;
 END$$
 
 DELIMITER ;
